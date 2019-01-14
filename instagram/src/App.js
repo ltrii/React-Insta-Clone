bd6f0b03104c62f2373b978f4495f42f8220cb89
ltrii <lelandtr@gmail.com>
2019-01-14 10:32:46 -0800

    Init

---
+++ instagram/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+import dummyData from './dummy-data';
+import PostsContainer from './components/PostsContainer/PostsContainer';
+import SearchBar from './components/SearchBar/SearchBarContainer';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: dummyData
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        Under construction...
+      </div>
+    );
+  }
+}
+
+export default App;
