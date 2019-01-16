cca8fbeb6089f2b83d0934b3e09b7a798577c9a8
ltrii <lelandtr@gmail.com>
2019-01-15 16:41:33 -0800

    Comments work!

--- instagram/src/components/CommentSection/CommentInput.js
+++ instagram/src/components/CommentSection/CommentInput.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CommentInput = props => {
   return (
-    <form>
+    <form onSubmit={props.submitComment}>
       <input type="text" placeholder="Add comment... " value={props.comment} onChange={props.changeComment}/>
     </form>
   );
